e050bcddf0867d216504e0cc883fddafdb8a4eee
sachin <jadhavsachinsj47@gmail.com>
2018-07-11 12:09:11 +0530

    assignment 2 commit

---
+++ src2-3/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles:['h1 { color:blue;}'],
+  
+  //styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'marvellous';
+}
